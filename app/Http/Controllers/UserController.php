f58a99e80bbc2bef04f20bed90037028ceed1afb
aalvia123 <115689780+aalvia123@users.noreply.github.com>
2024-03-14 12:16:24 +0700

    fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,14 +11,17 @@ class UserController extends Controller
 {
     public function index()
     {
-        // tambah data user dengan eloquent model
-        $data = [
-            'nama' => 'pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data); //update data user
 
-        //coba akses model UserModel
-        $user = UserModel::all(); //ambil semua data dari tabel m_user
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data);
+
+
+        $user = UserModel::all();
         return view('user', ['data' => $user]);
     }
 }
