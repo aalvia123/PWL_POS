d425221e036915a90b09f913f547b7fa7c858118
aalvia123 <115689780+aalvia123@users.noreply.github.com>
2024-03-14 11:24:12 +0700

    ELOQUENT ORM

---
+++ app/Models/UserModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserModel extends Model
+{
+    use HasFactory;
+
+    protected $table ='m_user';     //Mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $primaryKey = 'user_id'; // mendefinisikan primary key dari tabel yang digunakan
+
+    /**
+     * the atributes that are mass asignable
+     *
+     * @var array
+     */
+    protected $fillable = ['level_id', 'username', 'nama', 'password' ];    
+}
