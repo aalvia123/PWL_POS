4247fa43128d7c6083e7c7f27cb50d1d67efae99
aalvia123 <115689780+aalvia123@users.noreply.github.com>
2024-03-28 05:14:20 +0700

    Relationships

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,12 +9,7 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 
 {
-    public function index()
-    {
 
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
-    }
 
     public function tambah()
     {
@@ -59,6 +54,12 @@ class UserController extends Controller
         $user->delete();
 
         return redirect('/user');
+    }
+
+    public function index()
+    {
+        $user = UserModel::with('level')->get();
+        return view('user', ['data' => $user]);
     }
 
 
