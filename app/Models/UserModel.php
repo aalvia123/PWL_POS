e7bb0fc4727dbc653c071eab37c8e746bc10e258
aalvia123 <115689780+aalvia123@users.noreply.github.com>
2024-05-02 23:11:52 +0700

    JS 10 PRAK 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Auth\User as Authenticatable; // Perbaiki penambahan ini untuk mengimpor kelas Authenticatable
-use App\Models\LevelModel;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract; // Menambahkan impor untuk AuthenticatableContract
+use Tymon\JWTAuth\Contracts\JWTSubject; // Menghapus backslash (\) pada namespace Tymon\JWTAuth\Contracts\JWTSubject
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class UserModel extends Authenticatable // Ganti nama kelas ini dari Model menjadi Authenticatable
+class UserModel extends Authenticatable implements AuthenticatableContract, JWTSubject // Menambahkan AuthenticatableContract dan JWTSubject
 {
     use HasFactory;
 
@@ -19,5 +20,16 @@ class UserModel extends Authenticatable // Ganti nama kelas ini dari Model menja
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    // Implementasi dari JWTSubject
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
