fa935694c7de5127ccfe431b6fa069aed1fb2c18
aalvia123 <115689780+aalvia123@users.noreply.github.com>
2024-05-16 21:27:04 +0700

    Prak 4 Js 10

--- app/Http/Controllers/Api/LogoutController.php
+++ app/Http/Controllers/Api/LogoutController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use\Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class LogoutController extends Controller
 {
