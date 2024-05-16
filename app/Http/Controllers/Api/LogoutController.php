85d720cb12f37a9cac40d3a6e51936e1fd95d2fb
aalvia123 <115689780+aalvia123@users.noreply.github.com>
2024-05-16 14:11:10 +0700

    Prak 3 Js 10

--- app/Http/Controllers/Api/LogoutController.php
+++ app/Http/Controllers/Api/LogoutController.php
@@ -4,8 +4,22 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use\Tymon\JWTAuth\Facades\JWTAuth;
 
 class LogoutController extends Controller
 {
-    //
+    public function __invoke(Request $request)
+    {
+        //remove token
+        $removeToken = JWTAuth::invalidate(JWTAuth::getToken());
+
+        if($removeToken){
+            //retuurn response JSON
+            return response()->json([
+                'success' =>true,
+                'message' =>'Logout Berhasil !'
+            ]);
+        }
+    }
+
 }
