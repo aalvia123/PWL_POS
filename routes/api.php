4fce4495098d256263a0b4461267bf1df4d2a536
aalvia123 <115689780+aalvia123@users.noreply.github.com>
2024-06-06 22:05:43 +0700

    TUGAS JS 11

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,15 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\LogoutController;
 use App\Http\Controllers\Api\LevelController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\KategoriController;
 use App\Http\Controllers\Api\BarangController;
-use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\TransaksiController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,44 +21,50 @@ use App\Http\Controllers\Api\RegisterController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-Route::post('/register', 'App\Http\Controllers\Api\RegisterController@register')->name('register');
-
+Route::post('/register', RegisterController::class)->name('register');
 Route::post('/login', LoginController::class)->name('login');
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    //return $request->user();
+    });
+    Route::post('/logout', LogoutController::class)->name('logout');
 });
-Route::post('/logout', LogoutController::class)->name('logout');
-
+//level
 Route::get('levels', [LevelController::class, 'index']);
 Route::post('levels', [LevelController::class, 'store']);
 Route::get('levels/{level}', [LevelController::class, 'show']);
 Route::put('levels/{level}', [LevelController::class, 'update']);
 Route::delete('levels/{level}', [LevelController::class, 'destroy']);
 
+//user
 Route::get('users', [UserController::class, 'index']);
 Route::post('users', [UserController::class, 'store']);
 Route::get('users/{user}', [UserController::class, 'show']);
 Route::put('users/{user}', [UserController::class, 'update']);
 Route::delete('users/{user}', [UserController::class, 'destroy']);
 
-Route::get('kategoris', [KategoriController::class, 'index']);
-Route::post('kategoris', [KategoriController::class, 'store']);
-Route::get('kategoris/{kategori}', [KategoriController::class, 'show']);
-Route::put('kategoris/{kategori}', [KategoriController::class, 'update']);
-Route::delete('kategoris/{kategori}', [KategoriController::class, 'destroy']);
+//kategori
+Route::get('kategori', [KategoriController::class, 'index']);
+Route::post('kategori', [KategoriController::class, 'store']);
+Route::get('kategori/{kategori}', [KategoriController::class, 'show']);
+Route::put('kategori/{kategori}', [KategoriController::class, 'update']);
+Route::delete('kategori/{kategori}', [KategoriController::class, 'destroy']);
 
-Route::get('barangs', [BarangController::class, 'index']);
-Route::post('barangs', [BarangController::class, 'store']);
-Route::get('barangs/{barang}', [BarangController::class, 'show']);
-Route::put('barangs/{barang}', [BarangController::class, 'update']);
-Route::delete('barangs/{barang}', [BarangController::class, 'destroy']);
+//barang
+Route::get('barang', [BarangController::class, 'index']);
+Route::post('barang', [BarangController::class, 'store']);
+Route::get('barang/{barang}', [BarangController::class, 'show']);
+Route::put('barang/{barang}', [BarangController::class, 'update']);
+Route::delete('barang/{barang}', [BarangController::class, 'destroy']);
 
+//register1
 Route::post('/register1', RegisterController::class)->name('register1');
 
-
-//Route::post('register1', [RegisterController::class, 'register1']);
+//transaksi
+Route::get('transaksi', [TransaksiController::class, 'index']);
+Route::post('transaksi', [TransaksiController::class, 'store']);
+Route::get('transaksi/{transaksi}', [TransaksiController::class, 'show']);
+Route::put('transaksi/{transaksi}', [TransaksiController::class, 'update']);
+Route::delete('transaksi/{transaksi}', [TransaksiController::class, 'destroy']);
